b093a24dfd720e948989a69bb019438076eeaba2
Miguel Ángel Bermea Rdz <73300671+MaikiBR@users.noreply.github.com>
2022-03-05 13:52:02 -0600

    Source Code

---
+++ db.php
@@ -0,0 +1,30 @@
+<?php
+
+//Llamado al input del formulario
+$lugar = $_POST["lugar"];
+$fecha = $_POST["fecha"];
+$nombre = $_POST["nombre"];
+$sexo = $_POST["sexo"];
+$edad = $_POST["edad"];
+$ocupacion = $_POST["ocupacion"];
+$escuela = $_POST["escuela"];
+$nomb_den = $_POST["nomb_den"];
+$ocup_den = $_POST["ocup_den"];
+$descripcion = $_POST["descripcion"];
+$correo = $_POST["correo"];
+//EVIDENCIA $nombre = $_POST["nombre"];
+
+//Coneccion a DB
+$conn = new mysqli('localhost', 'root', '', 'testagnus');
+
+if($conn->connect_error){
+  die('Connection Failed : '.$conn->connect_error);
+}else{
+  $stmt = $conn->prepare("insert into registration(lugar, fecha, nombre, sexo, edad, ocupacion, escuela, nomb_den, ocup_den, descripcion, correo) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+  $stmt->bind_param("ssssissssss", $lugar,$fecha,$nombre,$sexo,$edad,$ocupacion,$escuela,$nomb_den,$ocup_den,$descripcion,$correo);
+  $stmt->execute();
+  echo "Registration Successfully...";
+  $stmt->close();
+  $conn->close();
+}
+?>
---
+++ mainscript.js
@@ -0,0 +1,9 @@
+document.getElementById("info").onclick = function () {
+  document.location.replace('./infoindex.html');
+};
+document.getElementById("den").onclick = function () {
+  document.location.replace('./denindex.html');
+};
+document.getElementById("dep").onclick = function () {
+  document.location.replace('./depindex.html');
+};
